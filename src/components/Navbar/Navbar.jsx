0eb2b4c193c9737dd09fefe66f8b055717a6d3a2
alicendeh <alicendeh16@gmail.com>
2022-06-19 14:09:33 +0100

    a

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import logo from '../../assets/logo.png';
+import style from './Navbar.module.css';
 
 function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className={`row ${style.main}`}>
+      <div className={`${style.container} col-6`}>
+        <div>
+          <img src={logo} width={'125px'} alt="App Logo" />
+        </div>
+        <ul className="row mt-4">
+          <li className="col">Home</li>
+          <li className="col">Features</li>
+          <li className="col">Waves</li>
+          <li className="col">Resources</li>
+        </ul>
+      </div>
+      <div className={`col-6 row mt-4 ${style.endItems}`}>
+        {/* <div className="col-2">Hello,</div> */}
+        <div className={`col-2 ${style.btn}`}>Get Started</div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
