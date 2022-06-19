5f06e54345b2d89c8049635c21d1540ffed20c0c
alicendeh <alicendeh16@gmail.com>
2022-06-19 13:42:42 +0100

    a

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './LandingPage.module.css';
+import Navbar from '../components/Navbar/Navbar';
+
+function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default LandingPage;
